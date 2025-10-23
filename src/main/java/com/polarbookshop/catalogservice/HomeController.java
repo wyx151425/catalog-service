6d3496d6a452962cb54fa1a8eaf72b183a7c062a
wyx151425 <wyx151425@163.com>
2025-10-23 22:14:50 +0800

    fix: 使用配置服务器外部化配置

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,5 +1,6 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping(value = "/")
-    public String getGreeting() {
-        return "Welcome to the book catalog!";
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
     }
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return polarProperties.getGreeting();
+    }
+
 }
